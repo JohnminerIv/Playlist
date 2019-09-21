6b03bd27def9f693bea0fcad0a1911e9ff5a699e
John Miner <john.douglas.miner.iv@gmail.com>
2019-09-21 10:46:28 -0700

    Done with page two

--- app.py
+++ app.py
@@ -2,11 +2,23 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+playlists = [
+    {"title": "cat videos", "description": "cats acting weird"},
+    {"title": "workout playlist", "description": "Get pumped!"}
+]
 
+"""
 @app.route('/')
 def index():
-    """Return homepage."""
+    # Return homepage.
     return render_template("home.html", message="flask is cool")
+"""
+
+
+@app.route('/')
+def playlists_index():
+    """Return playlists."""
+    return render_template("playlists_index.html", playlists=playlists)
 
 
 if __name__ == '__main__':
