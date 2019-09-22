ad4910b32a06c0b805ee472abdbfc615acdc0e06
John Miner <john.douglas.miner.iv@gmail.com>
2019-09-22 10:13:53 -0700

    Mongodb is being weird...

--- app.py
+++ app.py
@@ -1,13 +1,17 @@
 from flask import Flask, render_template
+from pymongo import MongoClient
 
+client = MongoClient()
+db = client.Playlister
+playlists = db.playlists
 app = Flask(__name__)
-
+"""
 playlists = [
     {"title": "cat videos", "description": "cats acting weird"},
     {"title": "workout playlist", "description": "Get pumped!"}
 ]
 
-"""
+
 @app.route('/')
 def index():
     # Return homepage.
@@ -18,7 +22,7 @@ def index():
 @app.route('/')
 def playlists_index():
     """Return playlists."""
-    return render_template("playlists_index.html", playlists=playlists)
+    return render_template("playlists_index.html", playlists=playlists.find())
 
 
 if __name__ == '__main__':
