18e0fd3e1a621233627129ceefa95b1e88826283
John Miner <john.douglas.miner.iv@gmail.com>
2019-09-28 14:49:58 -0700

    Uri

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from pymongo import MongoClient
 from bson.objectid import ObjectId
 import os
 
-host = os.environ.get('MONGODB_URI', 'mongodb://localhost:27017/Playlister')
+host = os.environ.get('MONGOLAB_PUCE_URI', 'mongodb://localhost:27017/Playlister')
 client = MongoClient(host=f'{host}?retryWrites=false')
 db = client.Playlister
 playlists = db.playlists
