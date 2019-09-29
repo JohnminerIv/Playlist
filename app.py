edbe8116b5f769e902ebcdf0a85349bbd57a5433
John Miner <john.douglas.miner.iv@gmail.com>
2019-09-28 18:21:53 -0700

    Users can make and see comments and delete maybe?

--- app.py
+++ app.py
@@ -100,7 +100,6 @@ def comments_delete(comment_id):
         raise NotFound()
 
 
-
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=os.environ.get('PORT', 5000))
     # app.run(debug=True)
